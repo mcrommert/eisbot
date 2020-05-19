9ff50f5efb5af2a0f69eefaa204b1bf94bf003a6
ssidhartrinity <62565794+ssidhartrinity@users.noreply.github.com>
2020-05-19 15:15:23 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -82,6 +82,7 @@ function initBotConversation() {
         botAvatarBackgroundColor: '#555659',
        // botAvatarImage: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEclA3Vh3sTNw/company-logo_200_200/0?e=2159024400&v=beta&t=KD7qFl36K5BdZmGGqu1k8uZD-wYIn47CW_nR5f0l1z4',
         // userAvatarImage: '',
+        richCardWrapTitle: true,
         hideSendBox: false, /* set to true to hide the send box from the view */
         botAvatarInitials: 'C',
         userAvatarInitials: 'You',
