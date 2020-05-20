fa483e4d1c9b788d0fbc32d73c344e9c9d033789
ssidhartrinity <62565794+ssidhartrinity@users.noreply.github.com>
2020-05-20 12:24:49 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -209,5 +209,83 @@ function adaptiveCardsOption(event) {
         }
     }
 }
+function grayButton(button) {
+    button.style.backgroundColor = "#d9d9d9";
+    button.style.color = "#ffffff";
+    button.height = "37px";
+}
+
+function blueButton(button) {
+    button.style.backgroundColor = "#0078d7";
+    button.style.color = "white";
+    button.height = "37px";
+}
+
+function disableParentButtons(children, targetButton) {
+    for (let i = 0; i < children.length; i++) {
+        var alreadhClicked = false;
+        for (var j = 0; j < children[i].classList.length; j++) {
+            if (children[i].classList[j] === "old-button" || children[i].classList[j] === "expandable") {
+                alreadhClicked = true;
+                break;
+            }
+        }
+        if (children[i].nodeName === "BUTTON" && !alreadhClicked) {
+            if (children[i].innerText) {
+                if (children[i].innerText !== targetButton) {
+                    grayButton(children[i]);
+                } else {
+                    blueButton(children[i]);
+                }
+                children[i].classList.remove("ac-pushButton");
+                children[i].classList.add("old-button");
+                setTimeout(function () {
+                    if (children[i] != null) {
+                        children[i].onclick = "null";
+                    }
+                }, 50);
+                children[i].removeEventListener("click", selectOption);
+                children[i].style.outline = "none";
+                children[i].style.cursor = "default";
+            }
+        }
+    }
+}
+
+function disableButtons(targetButton) {
+    var alreadyClicked = false;
+    for (var j = 0; j < targetButton.classList.length; j++) {
+        if (targetButton.classList[j] === "old-button" || targetButton.classList[j] === "expandable") {
+            alreadyClicked = true;
+            break;
+        }
+    }
+    for (var k = 0; k < targetButton.parentNode.classList.length; k++) {
+        if (targetButton.parentNode.classList[k] === "old-button" || targetButton.parentNode.classList[k] === "expandable") {
+            alreadyClicked = true;
+            break;
+        }
+    }
+    if (alreadyClicked) {
+        return;
+    }
+    blueButton(targetButton);
+    targetButton.classList.add("old-button");
+    targetButton.parentNode.parentNode.parentNode.parentNode.style.cursor = "not-allowed";
+    var allChildren = targetButton.parentNode.childNodes;
+    for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i].innerText) {
+            if (allChildren[i].innerText !== targetButton.innerText) {
+                grayButton(allChildren[i]);
+            }
+            allChildren[i].classList.remove("ac-pushButton");
+            allChildren[i].classList.add("old-button");
+            allChildren[i].onclick = "null";
+            allChildren[i].removeEventListener("click", selectOption);
+            allChildren[i].style.outline = "none";
+            allChildren[i].style.cursor = "default";
+        }
+    }
+}
 
 
